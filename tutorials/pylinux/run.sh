74826a7142cb13c28c92c7b36bfb83cac992bf16
Code Monkey King <freesoft.for.people@gmail.com>
2022-08-01 17:55:41 +0300

    updated run.sh for pylinux

--- tutorials/pylinux/run.sh
+++ tutorials/pylinux/run.sh
@@ -1,1 +1,6 @@
-cd root && rm -f root.cpio.gz && makecpio && cp root.cpio.gz ../root.cpio.gz && cd .. && runqemu
+cd root &&
+rm -f root.cpio.gz &&
+find . | cpio -o -H newc | gzip > root.cpio.gz &&
+cp root.cpio.gz ../root.cpio.gz &&
+cd .. &&
+qemu-system-x86_64 -nographic -no-reboot -kernel bzImage -initrd root.cpio.gz -append "panic=1 console=ttyS0"
